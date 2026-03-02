a8e75e1543153b2a001b4e4af52fa849561fa587
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-02 15:48:46 +0500

    Add question-12: Math expressions

--- chapter-05-math-expressions/question-12/app.js
+++ chapter-05-math-expressions/question-12/app.js
@@ -1,1 +1,9 @@
-// app.js for question-12 
+var rad = 20;
+var pi = 3.142;
+
+var circumference = 2 * pi * rad;
+var area = pi * (rad ** 2);
+
+document.writeln(`Radius of a circle: ${rad} <br />`);
+document.writeln(`The circumference: ${circumference} <br />`);
+document.writeln(`The area is: ${area}`);
