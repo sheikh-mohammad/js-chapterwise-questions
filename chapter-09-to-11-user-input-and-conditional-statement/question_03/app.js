8eb0676cf8db9e5fbd28af0e36f67de140c5e12c
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-06 14:47:12 +0500

    Add question 03 comment - traffic signal color table

--- chapter-09-to-11-user-input-and-conditional-statement/question_03/app.js
+++ chapter-09-to-11-user-input-and-conditional-statement/question_03/app.js
@@ -1,4 +1,10 @@
 // Question 3 - Chapter 09-11 User Input and Conditional Statement
 
-// 3. Write a program to take input color of road traffic signal
-// from the user & show the message according to this table:
+// 3. Write a program to take input color of road traffic signal from the user & show the message according to this table:
+
+// Signal color Message
+// Red    Must Stop
+// Yellow Ready to move
+// Green  Move now
+
+if 
