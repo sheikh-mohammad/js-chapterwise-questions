04ccb7487fe4ba261a3f7a3e727956df615c3ec7
Sheikh Mohammad Ahmed <sheikhmohammadahmed07@gmail.com>
2026-03-07 11:50:39 +0500

    Add solutions to questions 08-11 - divisibility, even/odd, temperature, and calculator

--- chapter-09-to-11-user-input-and-conditional-statement/question_11/app.js
+++ chapter-09-to-11-user-input-and-conditional-statement/question_11/app.js
@@ -1,1 +1,24 @@
-// Question 11 - Chapter 09-11 User Input and Conditional Statement
+var number1 = +prompt("Enter Number 1");
+var operator = prompt("Enter Operator (+, -, *, /, %)");
+var number2 = +prompt("Enter Number 2");
+
+var result = 0;
+
+if (operator === "+") {
+    result = number1 + number2;
+    alert(`${number1} ${operator} ${number2} = ${result}`)
+} else if (operator === "-") {
+    result = number1 - number2;
+    alert(`${number1} ${operator} ${number2} = ${result}`)
+} else if (operator === "*") {
+    result = number1 * number2;
+    alert(`${number1} ${operator} ${number2} = ${result}`)
+} else if (operator === "/") {
+    result = number1 / number2;
+    alert(`${number1} ${operator} ${number2} = ${result}`)
+} else if (operator === "%") {
+    result = number1 % number2;
+    alert(`${number1} ${operator} ${number2} = ${result}`)
+} else {
+    alert("ERROR!");
+}
